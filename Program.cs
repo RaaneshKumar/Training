6056ca81eaa99d704ee4540b918d0fa33cecd195
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-07 16:36:18 +0530

    Started T01

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿Console.Write ("Enter a decimal value: ");
+string userinput = Console.ReadLine ();
+int input = int.Parse (userinput);
+string hexvalue = input.ToString ("X");
+Console.WriteLine (hexvalue);
