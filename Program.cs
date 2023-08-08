ae10565cc59aa95f10e9bc0cf038d412433bdabb
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-08 10:09:38 +0530

    Created T01

--- Program.cs
+++ Program.cs
@@ -3,3 +3,12 @@ string userinput = Console.ReadLine ();
 int input = int.Parse (userinput);
 string hexvalue = input.ToString ("X");
 Console.WriteLine (hexvalue);
+string binarynumber = "";
+while (input > 0) {
+   int remainder = input % 2;
+   binarynumber = Convert.ToString (remainder) + binarynumber;
+   input = input / 2;
+}
+foreach (var item in binarynumber) {
+   Console.Write (item);
+}
