8f215cdc12adfaff310bc65128772e76a394d44f
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-09 09:53:58 +0530

    Created T06

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿// To check whether the given input is a palindrome or not
+
+Console.Write ("Enter something to check whether it is a palindrome or not: ");
+string userinput = Console.ReadLine ();
+string input = userinput.ToLower ();
+List<char> order = new List<char> ();
+for (int i = 0; i < input.Length; i++) {
+   if (input[i] != ' ') {
+      order.Add (input[i]);
+   }
+}
+List<char> reverseorder = Enumerable.Reverse (order).ToList ();
+for (int i = 0; i < order.Count; i++) {
+   if (order[i] == reverseorder[i]) {
+      if (i == order.Count - 1) {
+         Console.WriteLine ("The given input is a palindrome");
+      }
+      continue;
+   } else {
+      Console.WriteLine ("The given input is not a palindrome.");
+      break;
+   }
 }
