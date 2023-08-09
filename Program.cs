5559215a607691f9c49512d3818211a5c46835e0
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-09 09:36:43 +0530

    Created T04

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿// Find LCM and GCD of two numbers
+
+Console.Write ("Enter the first number: ");
+string first_number = Console.ReadLine ();
+Console.Write ("Enter the second number: ");
+string second_number = Console.ReadLine ();
+int number1 = int.Parse (first_number);
+int number2 = int.Parse (second_number);
+int[] multiples = new int[] { };
+for (int i = 2; i <= number1; i++) {
+   int lcm1 = number1 % i;
+   int lcm2 = number2 % i;
+   if (lcm1 == 0 && lcm2 == 0) {
+      multiples = multiples.Append (i).ToArray ();
+   }
 }
+int gcd = multiples.Max ();
+int lcm = (number1 * number2) / gcd;
+Console.WriteLine ("LCM of given numbers is {0}", lcm);
+Console.WriteLine ("GCD of given numbers is {0}", gcd);
