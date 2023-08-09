7deee02dd8c6327833f3a166d702c0efa9e178fa
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-09 09:50:45 +0530

    Created T05

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿// Check whether the given number is a Prime Number or not
+
+Console.Write ("Enter a number: ");
+string input = Console.ReadLine ();
+int given_number = int.Parse (input);
+if (given_number < 0) {
+   Console.WriteLine ("Negative Numbers cannot be prime.");
+}
+if (given_number is 0 or 1) {
+   Console.WriteLine ("{0} is neither prime nor composite.", given_number);
+}
+if (given_number > 2) {
+   for (int i = 2; i < given_number; i++) {
+      int result = given_number % i;
+      if (result == 0) {
+         Console.WriteLine ("{0} is not a prime number.", given_number);
+         break;
+      } if (i==given_number -1 ) {
+         Console.WriteLine ("{0} is a prime number.", given_number);
+         break;
+      }
+   }
+}
+if (given_number == 2) {
+   Console.WriteLine ("2 is a prime number");
 }
