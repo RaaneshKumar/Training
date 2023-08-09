cad3f547dfee51d5ea12f93a5bccbe9546962ca4
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-09 09:33:47 +0530

    Created T03

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿// Fibonacci series
+// Given an input eg:n=10, print fibonacci series for f(10)
+
+int first_number = 0;
+int second_number = 1;
+Console.Write ("Write the number of elements required for this fibonacci series: ");
+string input = Console.ReadLine ();
+Console.WriteLine (first_number);
+Console.WriteLine (second_number);
+int n = int.Parse (input);
+for (int i = 0; i < n; i++) {
+   int next_number = first_number + second_number;
+   Console.WriteLine (next_number);
+   first_number = second_number;
+   second_number = next_number;
 }
