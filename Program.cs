ffdff832ddb4939b5c9ee9d28d7b734052d455df
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-09 12:13:10 +0530

    Made change in T04

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ string second_number = Console.ReadLine ();
 int number1 = int.Parse (first_number);
 int number2 = int.Parse (second_number);
 int[] multiples = new int[] { };
-for (int i = 2; i <= number1; i++) {
+for (int i = 1; i <= number1; i++) {
    int lcm1 = number1 % i;
    int lcm2 = number2 % i;
    if (lcm1 == 0 && lcm2 == 0) {
