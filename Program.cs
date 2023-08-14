8b7113303de12a78c55cdefcb4a5807067847fcd
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-14 09:12:17 +0530

    Made changes

--- Program.cs
+++ Program.cs
@@ -1,20 +1,14 @@
 ﻿// Find LCM and GCD of two numbers
 
 Console.Write ("Enter the first number: ");
-string first_number = Console.ReadLine ();
+int number1 = int.Parse (Console.ReadLine ());
 Console.Write ("Enter the second number: ");
-string second_number = Console.ReadLine ();
-int number1 = int.Parse (first_number);
-int number2 = int.Parse (second_number);
+int number2 = int.Parse (Console.ReadLine ());
 int[] multiples = new int[] { };
 for (int i = 1; i <= number1; i++) {
-   int lcm1 = number1 % i;
-   int lcm2 = number2 % i;
-   if (lcm1 == 0 && lcm2 == 0) {
-      multiples = multiples.Append (i).ToArray ();
-   }
+   int lcm1 = number1 % i; int lcm2 = number2 % i;
+   if (lcm1 == 0 && lcm2 == 0) multiples = multiples.Append (i).ToArray ();
 }
 int gcd = multiples.Max ();
 int lcm = (number1 * number2) / gcd;
-Console.WriteLine ("LCM of given numbers is {0}", lcm);
-Console.WriteLine ("GCD of given numbers is {0}", gcd);
+Console.WriteLine ("LCM of given numbers is {0} \nGCD of given numbers is {1}", lcm , gcd);
