55ed7cec3f314126a18cb151c9668d680b7ceea5
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-16 17:50:26 +0530

    Created T11

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿// Write a program to find the digital root of a given number.
+
+Console.WriteLine ("Enter a number: ");
+int input = int.Parse (Console.ReadLine ());
+if (input == 0) Console.WriteLine ("The digital root is 0");
+else {
+   int remainder = input % 9;
+   if (remainder != 0) Console.WriteLine ($"The digital root is {remainder}");
+   else if (remainder == 0) Console.WriteLine ("The digital root is 9");
 }
