35215dffd7b8212bce931fcece62d43b775b929b
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-16 17:40:31 +0530

    Created T08

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿//Print multiplication tables from 1 to 10
+
+for (int i = 1; i <= 10; i++) {
+   for (int j = 1; j <= 10; j++) Console.WriteLine ($"{i} * {j} = {i * j}");
+   Console.WriteLine ();
 }
