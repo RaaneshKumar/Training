5d57f37954600c35f32d18cd2c5034031ea56ee0
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-17 11:41:42 +0530

    Made changes

--- Program.cs
+++ Program.cs
@@ -1,18 +1,29 @@
 ﻿// Convert decimal to hexadecimal and binary
 
 // To Hexadecimal
+  //string hexvalue = input.ToString ("X") ----> in-built function for reference
 
 Console.Write ("Enter a decimal value: ");
-int input = int.Parse (Console.ReadLine ());
-string hexvalue = input.ToString ("X");
-Console.WriteLine (hexvalue);
+if (int.TryParse (Console.ReadLine (), out int input)) return;
+else Console.WriteLine ("Invalid input");
+int input1 = input;
+string[] hexdigits = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+string hexvalue = "";
+if (input == 0) hexvalue= "0";
+while (input1 > 0) {
+   hexvalue = hexdigits[input1 % 16] + hexvalue;
+   input1 /= 16;
+} 
+Console.WriteLine ($"Hexadecimal: {hexvalue}");
+
 
 // To binary 
 
+input1 = input;
 string binarynumber = "";
-while (input > 0) {
-   int remainder = input % 2;
-   binarynumber = Convert.ToString (remainder) + binarynumber;
-   input = input / 2;
-}
-foreach (var item in binarynumber) Console.Write (item);
+if (input == 0) binarynumber = "0";
+while (input1 > 0) {
+   binarynumber = (input1 % 2) + binarynumber;
+   input1 /= 2;
+} 
+Console.Write ($"Binary: {binarynumber}");
