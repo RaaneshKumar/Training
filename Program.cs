4ecbe71fbf2eb6ee5f25164dcdc32515c8274c50
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-23 17:02:54 +0530

    Created T12

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿//Pascal's Triangle
+
+
+Console.Write ("Enter the number of rows: ");
+int.TryParse (Console.ReadLine (), out int n);
+int value = 1;
+for (int i = 0; i < n; i++) {
+   Console.Write (string.Concat (Enumerable.Repeat ("  ", (n - i))));
+   for (int j = 0; j <= i; j++) {
+      if (j == 0) {
+         Console.Write ("1 ");
+         value = 1;
+      } else {
+         value = value * (i - j + 1) / j;
+         Console.Write ($"{value,6}");
+      }
+   }
+   Console.WriteLine ();
 }
