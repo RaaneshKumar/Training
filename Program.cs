e3ec706b6a2e088041ed91c661c19728b1a17d4a
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-25 13:59:05 +0530

    Created T14

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿//Reduce a string of lowercase characters by deleting a pair of adjacent letters that match. 
+
+Console.WriteLine ("Enter a string: ");
+string user_input = Console.ReadLine ();
+user_input = user_input.ToLower();
+List<char> input = new List<char> () { };
+foreach (char item in user_input) {
+   input.Add (item);
 }
+for (int i = 1; i < input.Count; i++) {
+   if (input[i] == input[i - 1]) {
+      input.RemoveAt (i);
+      i--;
+   }
+}
+foreach (char item in input) Console.Write (item);
