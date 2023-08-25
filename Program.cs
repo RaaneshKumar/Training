60c6a4b02cb16584ee45a955643b681bfbe74969
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-25 14:07:55 +0530

    Created T15

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿//An isogram is a word that has no duplicate letters.
+//Create a function that takes a string and returns either true or false depending on whether it's an "isogram". 
+
+Console.WriteLine ("Enter a string: ");
+string input = Console.ReadLine ();
+string output = "";
+if (isogram () == true) Console.WriteLine ("It is an isogram.");
+else Console.WriteLine ("Not an isogram.");
+
+bool isogram () {
+   foreach (char item in input) {
+      if (output.Contains (item) == false) output += item;
+   }
+   if (input == output) return true;
+   return false;
 }
