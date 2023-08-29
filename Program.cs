d2c4ee139ec58a09ced1c65d23236ad19997e3f6
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-29 12:35:36 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -8,32 +8,31 @@ int number = random.Next (1, GetMax () + 1);
 Console.WriteLine ($"The computer has picked a number between 1 and {GetMax ()}. Guess it correctly if you can. ");
 for (int tries = 1; ; tries++) {
    Console.Write ("Enter your guess: ");
-   int.TryParse (Console.ReadLine (), out int guess);
-   if (guess == number) {
-      Console.WriteLine ($"Correct. You guessed it in {tries} tries.");
-      break;
-   } else if (guess > number) Console.WriteLine ("Too High");
-   else if (guess < number) Console.WriteLine ("Too Low");
-   else Console.WriteLine ("Invalid entry.");
+   if (int.TryParse (Console.ReadLine (), out int guess)) {
+      if (guess == number) {
+         Console.WriteLine ($"Correct. You guessed it in {tries} tries.");
+         break;
+      } else if (guess > number) Console.WriteLine ("Too High");
+      else Console.WriteLine ("Too Low");
+   } else Console.WriteLine ("Invalid input");
 }
 
 int GetMax () {
    switch (mode) {
-      case Level.Easy: return 10;
-      case Level.Hard: return 1000;
+      case ELevel.Easy: return 10;
+      case ELevel.Hard: return 1000;
       default: return 100;
    }
 }
-Level GetMode () {
+ELevel GetMode () {
    Console.WriteLine ("Choose the difficulty level: (E)asy , (M)edium , (H)ard ");
    for (; ; ) {
-      var difficulty = Console.ReadKey (true).Key;
-      switch (difficulty) {
-         case ConsoleKey.E: return Level.Easy;
-         case ConsoleKey.H: return Level.Hard;
-         case ConsoleKey.M: return Level.Medium;
+      switch (Console.ReadKey (true).Key) {
+         case ConsoleKey.E: return ELevel.Easy;
+         case ConsoleKey.H: return ELevel.Hard;
+         case ConsoleKey.M: return ELevel.Medium;
          default: Console.WriteLine ("Incorrect Choice."); break;
       }
    }
 }
-enum Level { Easy, Medium, Hard }
+enum ELevel { Easy, Medium, Hard }
