16f2b3caea1182e8c57ee3695190c5d4ed6a44c9
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-29 13:07:24 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -2,22 +2,18 @@
 //User thinks of a number between 0 to 127 and computer finds it in 7 tries.
 
 Console.WriteLine ("Think of a number between 0 to 127.");
-int max_limit = 128;
-int min_limit = 0;
-int average = (min_limit + max_limit) / 2;
+int maxLimit = 128; int minLimit = 0; int average = (minLimit + maxLimit) / 2;
 for (int i = 0; i < 7; i++) {
    Console.WriteLine ($"Is the number less than {average}? (Y)es or (N)o");
    var answer = Console.ReadKey (true).Key;
    if (answer == ConsoleKey.Y) {
-      max_limit = average;
-      average = (min_limit + max_limit) / 2;
+      maxLimit = average;
+      average = (minLimit + maxLimit) / 2;
    } else if (answer == ConsoleKey.N) {
-      min_limit = average;
-      average = (min_limit + max_limit) / 2;
+      minLimit = average;
+      average = (minLimit + maxLimit) / 2;
    } else {
-      Console.WriteLine ("Incorrect choice.");
-      i--;
-   }
+      Console.WriteLine ("Incorrect choice. Please try again.");
+      break;
+   } if (i == 6) Console.WriteLine ($"\nThe number you had in mind is {average}.");
 }
-Console.WriteLine ($"\nThe number you had in mind is {average}.");
-
