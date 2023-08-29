eb3cb3a7b3a6ac33f74ac9aebd6298cf68eb07f3
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-29 10:28:07 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
 ﻿// Check whether the given number is a Prime Number or not
 
 Console.Write ("Enter a number: ");
-int given_number = int.Parse (Console.ReadLine ());
-if (given_number < 0) Console.WriteLine ("Negative Numbers cannot be prime.");
-if (given_number is 0 or 1) Console.WriteLine ("{0} is neither prime nor composite.", given_number);
-if (given_number > 2) {
-   for (int i = 2; i < given_number; i++) {
-      int result = given_number % i;
-      if (result == 0) {
-         Console.WriteLine ("{0} is not a prime number.", given_number);
-         break;
-      } if (i==given_number -1 ) {
-         Console.WriteLine ("{0} is a prime number.", given_number);
-         break;
+if (int.TryParse (Console.ReadLine (), out int givenNumber)) {
+   if (givenNumber < 0) Console.WriteLine ("Negative Numbers cannot be prime.");
+   if (givenNumber is 0 or 1) Console.WriteLine ("{0} is neither prime nor composite.", givenNumber);
+   if (givenNumber > 2) {
+      for (int i = 2; i < givenNumber / 2; i++) {
+         int result = givenNumber % i;
+         if (result == 0) {
+            Console.WriteLine ("{0} is not a prime number.", givenNumber);
+            break;
+         }
+         if (i == givenNumber / 2 - 1) {
+            Console.WriteLine ("{0} is a prime number.", givenNumber);
+            break;
+         }
       }
    }
-}
-if (given_number == 2) Console.WriteLine("2 is a prime number");
+   if (givenNumber == 2) Console.WriteLine ("2 is a prime number");
+} else Console.WriteLine ("Invlaid input");
