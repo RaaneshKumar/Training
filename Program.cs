be31f4ac40da48402809dc136af11cf86c2ec336
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-29 11:42:20 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿//Print multiplication tables from 1 to 10
 
 for (int i = 1; i <= 10; i++) {
-   for (int j = 1; j <= 10; j++) Console.WriteLine ($"{i} * {j} = {i * j}");
+   for (int j = 1; j <= 10; j++) Console.WriteLine ($"{i} * {j,2} = {i * j}");
    Console.WriteLine ();
 }
