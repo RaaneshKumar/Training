238db925a1661b4a2146c2b060d46874c85f1ca2
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-29 09:43:40 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -1,29 +1,26 @@
 ﻿// Convert decimal to hexadecimal and binary
-
-// To Hexadecimal
-  //string hexvalue = input.ToString ("X") ----> in-built function for reference
+//string hexvalue = input.ToString ("X") ----> in-built function for reference
 
 Console.Write ("Enter a decimal value: ");
-if (int.TryParse (Console.ReadLine (), out int input)) ;
+if (int.TryParse (Console.ReadLine (), out int input)) {
+   //To hexadecimal
+   int input1 = input;
+   string[] hexdigits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+   string hexvalue = "";
+   if (input == 0) hexvalue = "0";
+   while (input1 > 0) {
+      hexvalue = hexdigits[input1 % 16] + hexvalue;
+      input1 /= 16;
+   }
+   Console.WriteLine ($"Hexadecimal: {hexvalue}");
+   //To binary
+   input1 = input;
+   string binarynumber = "";
+   if (input == 0) binarynumber = "0";
+   while (input1 > 0) {
+      binarynumber = (input1 % 2) + binarynumber;
+      input1 /= 2;
+   }
+   Console.Write ($"Binary: {binarynumber}");
+}
 else Console.WriteLine ("Invalid input");
-int input1 = input;
-string[] hexdigits = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
-string hexvalue = "";
-if (input == 0) hexvalue= "0";
-while (input1 > 0) {
-   hexvalue = hexdigits[input1 % 16] + hexvalue;
-   input1 /= 16;
-} 
-Console.WriteLine ($"Hexadecimal: {hexvalue}");
-
-
-// To binary 
-
-input1 = input;
-string binarynumber = "";
-if (input == 0) binarynumber = "0";
-while (input1 > 0) {
-   binarynumber = (input1 % 2) + binarynumber;
-   input1 /= 2;
-} 
-Console.Write ($"Binary: {binarynumber}");
