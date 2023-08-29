a4feabd84083d3604c92ceada14c277eb2537dd9
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-29 10:56:22 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿// Find LCM and GCD of two numbers
 
 Console.Write ("Enter the first number: ");
-int number1 = int.Parse (Console.ReadLine ());
+bool num1 = int.TryParse (Console.ReadLine (), out int number1);
 Console.Write ("Enter the second number: ");
-int number2 = int.Parse (Console.ReadLine ());
-int[] multiples = new int[] { };
-for (int i = 1; i <= number1; i++) {
-   int lcm1 = number1 % i; int lcm2 = number2 % i;
-   if (lcm1 == 0 && lcm2 == 0) multiples = multiples.Append (i).ToArray ();
-}
-int gcd = multiples.Max ();
-int lcm = (number1 * number2) / gcd;
-Console.WriteLine ("LCM of given numbers is {0} \nGCD of given numbers is {1}", lcm , gcd);
+bool num2 = int.TryParse (Console.ReadLine (), out int number2);
+if (num1 == true && num2 == true) {
+   int gcd = 0;
+   if (number1 == 0) Console.WriteLine ($"LCM of given numbers is 0. \nGCD of given numbers is {number2}.");
+   if (number1 > 0) {
+      for (int i = 1; i <= number1; i++) {
+         int lcm1 = number1 % i; int lcm2 = number2 % i;
+         if (lcm1 == 0 && lcm2 == 0 && i > gcd) gcd = i;
+      }
+      int lcm = (number1 * number2) / gcd;
+      Console.WriteLine ("LCM of given numbers is {0}. \nGCD of given numbers is {1}.", lcm, gcd);
+   }
+} else Console.WriteLine ("Invalid Input.");
