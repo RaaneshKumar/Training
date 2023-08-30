3f8ce5652eb5691f7867b9de797c6dc5f3205b58
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-30 10:57:55 +0530

    Changes Done

--- Program.cs
+++ Program.cs
@@ -1,132 +1,96 @@
 ﻿Console.Write ("Enter a number: ");
-int num = int.Parse (Console.ReadLine ());
-Console.Write ("If you want to convert into words, enter w. If you want to convert to roman numerals, enter rn. (Condition: Limit to convert to words is 10 crores. Limit to convert to roman numerals is 3999. Enter your choice: ");
-string choice = Console.ReadLine ();
-if (choice == "w") {
-   if (num > 100000000) Console.WriteLine ("Enter value within the limit."); else Console.WriteLine(to_words(num));
-}
-if (choice == "rn") {
-   if (num > 3999) Console.WriteLine("Enter value within the limit."); else Console.WriteLine(to_roman_numerals(num));
-}
+if (int.TryParse (Console.ReadLine (), out int num)) {
+   Console.Write ("If you want to convert into words, enter w. If you want to convert to roman numerals, enter r. (Condition: Limit to convert to words is 10 crores. Limit to convert to roman numerals is 3999. Enter your choice: ");
+   string choice = Console.ReadLine ();
+   if (choice == "w") {
+      if (num > 100000000) Console.WriteLine ("Enter value within the limit."); else Console.WriteLine (ToWords (num));
+   } else if (choice == "r") {
+      if (num > 3999) Console.WriteLine ("Enter value within the limit."); else Console.WriteLine (ToRomanNumerals (num));
+   } else Console.WriteLine ("Invlaid Input.");
+} else Console.WriteLine ("Invalid Input");
 
 // To print the given number in words
 // For example: 376 must be printed as three hundred and seventy six
 // Code is written for user input upto 10 crores
 
-string to_words (int num) {
-   string number_in_words = "";
+string ToWords (int num) {
+   string numberInWords = "";
    while (num > 0) {
       if (num >= 10000000) {
          int quotient = num / 10000000; int remainder = num % 10000000;
-         one_to_nineteen (quotient);
-         Console.Write ((one_to_nineteen (quotient)) + " crores ");
+         Console.Write ((OneToNinetyNine (quotient)) + " crores ");
          num = remainder;
       }
       if (num >= 100000) {
          int quotient = num / 100000; int remainder = num % 100000;
-         one_to_nineteen (quotient);
-         Console.Write ((one_to_nineteen (quotient)) + " lakhs ");
+         Console.Write ((OneToNinetyNine (quotient)) + " lakhs ");
          num = remainder;
       }
       if (num >= 1000) {
          int quotient = num / 1000; int remainder = num % 1000;
-         one_to_nineteen (quotient);
-         Console.Write ((one_to_nineteen (quotient)) + " thousand ");
+         Console.Write ((OneToNinetyNine (quotient)) + " thousand ");
          num = remainder;
       }
       if (num >= 100) {
          int quotient = num / 100; int remainder = num % 100;
-         one_to_nineteen (quotient);
-         Console.Write ((one_to_nineteen (quotient)) + " hundred and ");
+         Console.Write ((OneToNinetyNine (quotient)) + " hundred and ");
          num = remainder;
       }
-      if (num >= 0) {
-         one_to_nineteen (num);
-         Console.Write ((one_to_nineteen (num)));
-      }
-      Console.WriteLine (number_in_words);
+      if (num >= 0) Console.Write ((OneToNinetyNine (num)));
+      Console.WriteLine (numberInWords);
       break;
    }
 
 
-   string one_to_nineteen (int num1) {
+   string OneToNinetyNine (int num1) {
       string quotientstring = "";
       if (num1 >= 20) {
          int quotient1 = num1 / 10; int remainder1 = num1 % 10;
-         if (quotient1 == 2) quotientstring = " twenty ";
-         if (quotient1 == 3) quotientstring = " thirty ";
-         if (quotient1 == 4) quotientstring = " forty ";
-         if (quotient1 == 5) quotientstring = " fifty ";
-         if (quotient1 == 6) quotientstring = " sixty ";
-         if (quotient1 == 7) quotientstring = " seventy ";
-         if (quotient1 == 8) quotientstring = " eighty ";
-         if (quotient1 == 9) quotientstring = " ninety ";
+         Dictionary<int,string> twentyToNinety = new Dictionary<int,string> () { { 2, "twenty "}, { 3, "thirty " }, { 4, "forty " }, { 5, "fifty " }, { 6, "sixty " }, { 7, "seventy " }, { 8, "eighty " }, { 9, "ninety " } };
+         quotientstring = twentyToNinety[quotient1];
          num1 = remainder1;
       }
-      if (num1 == 1) quotientstring = quotientstring + "one";
-      if (num1 == 2) quotientstring = quotientstring + "two";
-      if (num1 == 3) quotientstring = quotientstring + "three";
-      if (num1 == 4) quotientstring = quotientstring + "four";
-      if (num1 == 5) quotientstring = quotientstring + "five";
-      if (num1 == 6) quotientstring = quotientstring + "six";
-      if (num1 == 7) quotientstring = quotientstring + "seven";
-      if (num1 == 8) quotientstring = quotientstring + "eight";
-      if (num1 == 9) quotientstring = quotientstring + "nine";
-      if (num1 == 10) quotientstring = quotientstring + "ten";
-      if (num1 == 11) quotientstring = quotientstring + "eleven";
-      if (num1 == 12) quotientstring = quotientstring + "twelve";
-      if (num1 == 13) quotientstring = quotientstring + "thirteen";
-      if (num1 == 14) quotientstring = quotientstring + "fourteen";
-      if (num1 == 15) quotientstring = quotientstring + "fifteen";
-      if (num1 == 16) quotientstring = quotientstring + "sixteen";
-      if (num1 == 17) quotientstring = quotientstring + "seventeen";
-      if (num1 == 18) quotientstring = quotientstring + "eighteen";
-      if (num1 == 19) quotientstring = quotientstring + "nineteen";
+      Dictionary<int, string> oneToNineteen = new Dictionary<int, string> () { { 1, "one" }, { 2, "two" }, { 3, "three" }, { 4, "four" }, { 5, "five" }, { 6, "six" }, { 7, "seven" }, { 8, "eight" }, { 9, "nine" }, { 10, "ten" }, { 11, "eleven" }, { 12, "twelve" }, { 13, "thirteen" }, { 14, "fourteen" }, { 15, "fifteen" }, { 16, "sixteen" }, { 17, "seventeen" }, { 18, "eighteen" }, { 19, "nineteen" } };
+      quotientstring += oneToNineteen[num1];
       return quotientstring;
    }
-   return number_in_words;
+   return numberInWords;
 }
+
 // Convert a given number to its corresponding roman numerals
 // Code is written for User input less than 4000
 
-string to_roman_numerals (int input) {
-   string number_in_rn = "";
+string ToRomanNumerals (int input) {
+   string numberInRoman = "";
    while (input > 0) {
       if (input > 1000) {
          int quotient = input / 1000; int remainder = input % 1000;
-         number_in_rn = string.Concat (Enumerable.Repeat ("M", quotient));
+         numberInRoman = string.Concat (Enumerable.Repeat ("M", quotient));
          input = remainder;
       }
       if (input > 100) {
          int quotient = input / 100; int remainder = input % 100;
-         if (quotient == 9) number_in_rn += "CM";
-         if (quotient > 5 && quotient < 9) number_in_rn += "D" + string.Concat (Enumerable.Repeat ("C", quotient - 5));
-         if (quotient == 5) number_in_rn += "D";
-         if (quotient == 4) number_in_rn += "CD";
-         if (quotient >= 1 && quotient < 4) number_in_rn += string.Concat(Enumerable.Repeat("C", quotient));
+         if (quotient == 9) numberInRoman += "CM";
+         if (quotient > 5 && quotient < 9) numberInRoman += "D" + string.Concat (Enumerable.Repeat ("C", quotient - 5));
+         if (quotient == 5) numberInRoman += "D";
+         if (quotient == 4) numberInRoman += "CD";
+         if (quotient >= 1 && quotient < 4) numberInRoman += string.Concat(Enumerable.Repeat("C", quotient));
          input = remainder;
       }
       if (input > 10) {
          int quotient = input / 10; int remainder = input % 10;
-         if (quotient == 9) number_in_rn += "XC";
-         if (quotient > 5 && quotient < 9) number_in_rn += "L" + string.Concat(Enumerable.Repeat("X", quotient - 5));
-         if (quotient == 5) number_in_rn += "L";
-         if (quotient == 4) number_in_rn += "XL";
-         if (quotient >= 1 && quotient < 4) number_in_rn += string.Concat(Enumerable.Repeat("X", quotient));
+         if (quotient == 9) numberInRoman += "XC";
+         if (quotient > 5 && quotient < 9) numberInRoman += "L" + string.Concat(Enumerable.Repeat("X", quotient - 5));
+         if (quotient == 5) numberInRoman += "L";
+         if (quotient == 4) numberInRoman += "XL";
+         if (quotient >= 1 && quotient < 4) numberInRoman += string.Concat(Enumerable.Repeat("X", quotient));
          input = remainder;
       }
       if (input >= 1) {
-         if (input == 9) number_in_rn += "IX";
-         if (input == 8) number_in_rn += "VIII";
-         if (input == 7) number_in_rn += "VII";
-         if (input == 6) number_in_rn += "VI";
-         if (input == 5) number_in_rn += "V";
-         if (input == 4) number_in_rn += "IV";
-         if (input == 3) number_in_rn += "III";
-         if (input == 2) number_in_rn += "II";
-         if (input == 1) number_in_rn += "I";
+         Dictionary<int,string> romanNumbers = new Dictionary<int, string> () { {1, "I"}, { 2, "II" }, { 3, "III" }, { 4, "IV" }, { 5, "V" }, { 6, "VI" }, { 7, "VII" }, { 8, "VIII" }, { 9, "IX" }, { 10, "X" } };
+         numberInRoman += romanNumbers[input];
       }
       break;
    }
-   return number_in_rn; 
+   return numberInRoman; 
 }
