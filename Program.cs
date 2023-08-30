a9568be71f6ba1d8318bf49226fe91943e8ad3b9
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-30 09:39:21 +0530

    Made changes

--- Program.cs
+++ Program.cs
@@ -3,28 +3,27 @@
 //If no word in an array matches the criteria, return an empty string. 
 
 Console.WriteLine ("Enter no. words to check the longest abecedarian word: ");
-int.TryParse (Console.ReadLine (), out int count);
+bool input = int.TryParse (Console.ReadLine (), out int count);
 List<string> words = new List<string> ();
 string sorted = "";
 List<string> abecedarian = new List<string> ();
-string result = "";
-
-
-for (int i = 0; i < count; i++) {
-   Console.Write ("Enter a string: ");
-   string word = Console.ReadLine ();
-   words.Add (word);
-}
-foreach (string each_word in words) {
-   List<char> letters = new List<char> ();
-   foreach (char item in each_word) letters.Add (item);
-   letters.Sort ();
-   foreach (char item in letters) sorted += item;
-   if (each_word == sorted) abecedarian.Add (sorted);
-   sorted = "";
-}
-if (abecedarian.Count > 0) {
-   result = abecedarian[0];
-   for (int i = 1; i < abecedarian.Count; i++) if (abecedarian[i].Length > result.Length) result = abecedarian[i];
-   Console.WriteLine (result);
-}
+if (input == true) {
+   for (int i = 0; i < count; i++) {
+      Console.Write ("Enter a string: ");
+      string word = Console.ReadLine ();
+      words.Add (word);
+   }
+   foreach (string each_word in words) {
+      List<char> letters = new List<char> ();
+      foreach (char item in each_word) letters.Add (item);
+      letters.Sort ();
+      foreach (char item in letters) sorted += item;
+      if (each_word == sorted) abecedarian.Add (sorted);
+      sorted = "";
+   }
+   if (abecedarian.Count > 0) {
+      string result = abecedarian[0];
+      for (int i = 1; i < abecedarian.Count; i++) if (abecedarian[i].Length > result.Length) result = abecedarian[i];
+      Console.WriteLine (result);
+   }
+} else Console.WriteLine ("Invalid input.");
