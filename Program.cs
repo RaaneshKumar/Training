4e3046add948818300149a8368dd44eac0a6f5cd
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-30 12:41:40 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -2,18 +2,19 @@
 
 
 Console.Write ("Enter the number of rows: ");
-int.TryParse (Console.ReadLine (), out int n);
-int value = 1;
-for (int i = 0; i < n; i++) {
-   Console.Write (string.Concat (Enumerable.Repeat ("  ", (n - i))));
-   for (int j = 0; j <= i; j++) {
-      if (j == 0) {
-         Console.Write ("1 ");
-         value = 1;
-      } else {
-         value = value * (i - j + 1) / j;
-         Console.Write ($"{value,6}");
+if (int.TryParse (Console.ReadLine (), out int n)) {
+   int value = 1;
+   for (int i = 0; i < n; i++) {
+      Console.Write (string.Concat (Enumerable.Repeat (" ", (n - i))));
+      for (int j = 0; j <= i; j++) {
+         if (j == 0) {
+            Console.Write ("1 ");
+            value = 1;
+         } else {
+            value = value * (i - j + 1) / j;
+            Console.Write ($"{value} ");
+         }
       }
+      Console.WriteLine ();
    }
-   Console.WriteLine ();
-}
+}else Console.WriteLine ("Invalid Input.");
