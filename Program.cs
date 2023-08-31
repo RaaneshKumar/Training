cae27143f8c7e50118c1e7b93a42ae01fed8ef8f
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-31 17:32:24 +0530

    Created T17

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿//Generate all string character permutations
+
+Console.Write ("Enter a string: ");
+string input = Console.ReadLine();
+char[] word = input.ToCharArray ();
+Permutation (word, 0, input.Length);
+void Permutation (char[] array, int a, int l) {
+   if (a == l) Console.WriteLine (array);
+   else {
+      for (int i = a; i < l; i++) {
+         Swap (ref array[a], ref array[i]);
+         Permutation (array, a + 1, l);
+      }
+   }
+
 }
+
+void Swap (ref char a, ref char b) { (a, b) = (b, a); }
