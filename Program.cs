339444e913ce980eb6b402a05b188b488a9528a9
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-08-31 10:41:16 +0530

    Made changes

--- Program.cs
+++ Program.cs
@@ -4,12 +4,10 @@ Console.WriteLine ("Enter a string: ");
 string user_input = Console.ReadLine ();
 user_input = user_input.ToLower();
 List<char> input = new List<char> () { };
-foreach (char item in user_input) {
-   input.Add (item);
-}
+foreach (char item in user_input) input.Add (item);
 for (int i = 1; i < input.Count; i++) {
    if (input[i] == input[i - 1]) {
-      input.RemoveAt (i);
+      input.RemoveAt (i); input.RemoveAt (i - 1);
       i--;
    }
 }
