5c06a4b0eedc3359de5c7d89e0513ff5916b6e62
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-04 10:49:46 +0530

    T18 : Armstrong Number

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿Console.WriteLine (armstrong ());
+
+string armstrong () {
+   Console.Write ("Enter a number to check whether it is an armstrong number or not: ");
+   if (int.TryParse(Console.ReadLine(), out int input)){
+      int temp = input;
+      double sum = 0;
+      while (temp > 0) {
+         double digit = temp % 10;
+         sum += Math.Pow (digit, input.ToString ().Length);
+         temp /= 10;
+      }
+      return (input == sum) ? "It is an armstrong number." : "It is not an armstrong number.";
+   }else { return ("Invlaid Input."); }
 }
