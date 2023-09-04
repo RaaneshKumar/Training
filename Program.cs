db17f943bab146f23eeff1da4558a2d7ee6cab74
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-04 11:58:09 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 string armstrong () {
    Console.Write ("Enter a number to check whether it is an armstrong number or not: ");
-   if (int.TryParse(Console.ReadLine(), out int input)){
+   if (int.TryParse (Console.ReadLine (), out int input)) {
       int temp = input;
       double sum = 0;
       while (temp > 0) {
@@ -11,5 +11,5 @@ string armstrong () {
          temp /= 10;
       }
       return (input == sum) ? "It is an armstrong number." : "It is not an armstrong number.";
-   }else { return ("Invlaid Input."); }
+   } else { return ("Invlaid Input."); }
 }
