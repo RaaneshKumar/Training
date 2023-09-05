94a81813bc03700bf35218da2becb3ab21ffa8cf
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-05 09:55:56 +0530

    T18 : nth Armstrong number

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿namespace Training {
+   internal class Program {
+      static void Main (string[] args) {
+         if (int.TryParse (args[0], out int input)) {
+            int count = 0;
+            for (int i = 1; ; i++) {
+               double sum = 0;
+               int temp = i;
+               double len = i.ToString ().Length;
+               while (temp > 0) {
+                  double digit = temp % 10;
+                  sum += Math.Pow (digit, len);
+                  temp /= 10;
+               }
+               if (sum == i) count++;
+               if (count == input) {
+                  Console.WriteLine (i);
+                  break;
+               }
+            }
+         } else Console.WriteLine ("Invalid Input.");
+      }
+   }
+} 
