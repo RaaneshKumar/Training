1e4f39f87951a9ad0a5380840bceaf6bdb767e95
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-05 10:08:31 +0530

    T19 : Factorial number

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// To print the factorial of a given number.
+
+Console.WriteLine (factorial ());
+string factorial () {
+   Console.Write ("Enter a number to print its factorial: ");
+   if (int.TryParse (Console.ReadLine (), out int input)) {
+      int factorial = 1;
+      for (int i = 1; i <= input; i++) {
+         factorial *= i;
+      }
+      return ($"Factorial of {input} is {factorial}");
+   } else return ("Ivalid Input.");
+} 
