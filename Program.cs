729da66d41643510438592ddd55101b86b196a09
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-12 09:45:05 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿// To print the factorial of a given number.
 
-Console.WriteLine (factorial ());
-string factorial () {
-   Console.Write ("Enter a number to print its factorial: ");
-   if (int.TryParse (Console.ReadLine (), out int input)) {
-      int factorial = 1;
-      for (int i = 1; i <= input; i++) {
-         factorial *= i;
-      }
-      return ($"Factorial of {input} is {factorial}");
-   } else return ("Ivalid Input.");
+Console.Write ("Enter a number to print its factorial: ");
+if (int.TryParse (Console.ReadLine (), out int input)) {
+   Console.WriteLine ($"Factorial of {input} is {Factorial (input)}.");
+} else Console.WriteLine ("Invalid Input.");
+
+int Factorial (int number) {
+   int factorial = 1;
+   for (int i = 1; i <= input; i++) {
+      factorial *= i;
+   }
+   return factorial;
 } 
