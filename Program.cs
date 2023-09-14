665c69f9373200d817b7eb95ddb3f021400cdab9
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-14 16:43:33 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿// To check whether the given input is a palindrome or not
 
 Console.Write ("Enter something to check whether it is a palindrome or not: ");
-string userinput = Console.ReadLine (); string trimmedInput = userinput.Replace(" ","").ToLower ();
+string userinput = Console.ReadLine (); string trimmedInput = userinput.Replace (" ", "").ToLower ();
 int n = trimmedInput.Length;
-for (int i = 0; i <= n / 2; i++) {
-   if (trimmedInput[i] != trimmedInput[n - 1 - i]) {
-      Console.WriteLine ("The given input is not a palindrome");
-      break;
-   } else {if (i == n / 2) Console.WriteLine ("The given input is a palindrome."); }
+Console.WriteLine ($"The given input is {(IsPalindrome () ? "" : "not ")}a palindrome");
+
+bool IsPalindrome () {
+   for (int i = 0; i <= n / 2; i++)
+      if (trimmedInput[i] != trimmedInput[n - 1 - i]) return false;
+   return true;
 }
