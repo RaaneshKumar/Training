ce69906cc146e1f87e9418d480e1f541f4157cc5
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-14 16:49:39 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 Console.Write ("Enter something to check whether it is a palindrome or not: ");
 string userinput = Console.ReadLine (); string trimmedInput = userinput.Replace (" ", "").ToLower ();
-int n = trimmedInput.Length;
+int length = trimmedInput.Length;
 Console.WriteLine ($"The given input is {(IsPalindrome () ? "" : "not ")}a palindrome");
 
 bool IsPalindrome () {
-   for (int i = 0; i <= n / 2; i++)
-      if (trimmedInput[i] != trimmedInput[n - 1 - i]) return false;
+   for (int i = 0; i <= length / 2; i++)
+      if (trimmedInput[i] != trimmedInput[length - 1 - i]) return false;
    return true;
 }
