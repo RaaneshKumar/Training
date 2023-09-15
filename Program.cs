3fad8b825f418c184f67dd460c1b1982f96a0259
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-15 14:35:42 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -4,15 +4,14 @@ Console.Write ("Enter the first number: ");
 bool num1 = int.TryParse (Console.ReadLine (), out int number1);
 Console.Write ("Enter the second number: ");
 bool num2 = int.TryParse (Console.ReadLine (), out int number2);
-if (num1 == true && num2 == true) {
+if (!(num1 && num2)) Console.WriteLine ("Invalid Input.");
+else {
    int gcd = 0;
-   if (number1 == 0) Console.WriteLine ($"LCM of given numbers is 0. \nGCD of given numbers is {number2}.");
-   if (number1 > 0) {
-      for (int i = 1; i <= number1; i++) {
-         int lcm1 = number1 % i; int lcm2 = number2 % i;
-         if (lcm1 == 0 && lcm2 == 0 && i > gcd) gcd = i;
-      }
-      int lcm = (number1 * number2) / gcd;
-      Console.WriteLine ("LCM of given numbers is {0}. \nGCD of given numbers is {1}.", lcm, gcd);
+   if (number1 < 0 || number2 < 0) Console.WriteLine ("Please enter non-negative integers.");
+   else if (number1 == 0) Console.WriteLine ($"\nLCM of given numbers is 0. \nGCD of given numbers is {number2}.");
+   else {
+      for (int i = 1; i <= number1; i++)
+         if (number1 % i == 0 && number2 % i == 0 && i > gcd) gcd = i;
+      Console.WriteLine ($"\nLCM of given numbers is {number1 * number2 / gcd}. \nGCD of given numbers is {gcd}.");
    }
-} else Console.WriteLine ("Invalid Input.");
+}
