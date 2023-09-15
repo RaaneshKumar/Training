f4523a52c2048664dbd80b3d726c06796338df4d
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-15 16:38:50 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -2,14 +2,16 @@
 // The input can be the number of rows (half the height).
 
 Console.Write ("Enter the number of rows: ");
-int n = int.Parse (Console.ReadLine ());
-int stars = 1; int space = n + 1;
-for (int i = 1; i <= n + 1; i++) {
-   Console.WriteLine (string.Concat (Enumerable.Repeat (" ", space)) + string.Concat (Enumerable.Repeat ("*", stars)));
-   stars = stars + 2; space = space - 1;
-}
-stars = stars - 4;
-for (int i = 1; i <= n; i++) {
-   Console.WriteLine (string.Concat (Enumerable.Repeat (" ", i + 1)) + string.Concat (Enumerable.Repeat ("*", stars)));
-   stars = stars - 2; space = space + 1;
+if (!int.TryParse (Console.ReadLine (), out int n)) Console.WriteLine ("Invalid Input.");
+else {
+   int stars = 1, space = n + 1;
+   for (int i = 1; i <= n + 1; i++) {
+      Console.WriteLine (new string (' ', space) + new string ('*', stars));
+      stars += 2; space -= 1;
+   }
+   stars -= 4;
+   for (int i = 1; i <= n; i++) {
+      Console.WriteLine (new string (' ', i + 1) + new string ('*', stars));
+      stars -= 2; space += 1;
+   }
 }
