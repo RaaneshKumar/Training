46299e68c365a56c0dc13c115e2f11c003950243
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-15 09:28:43 +0530

    Made changes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿// Check whether the given number is a Prime Number or not
 
 Console.Write ("Enter a number: ");
-if (!int.TryParse (Console.ReadLine (), out int givenNumber)) { Console.WriteLine ("Invlaid input"); }
-if (givenNumber < 0) Console.WriteLine ("Negative Numbers cannot be prime.");
-else if (givenNumber is 0 or 1) Console.WriteLine ($"{givenNumber} is neither prime nor composite.");
-else if (givenNumber == 2) Console.WriteLine ("2 is a prime number");
-else Console.WriteLine ($"{givenNumber} is {(IsPrime (givenNumber) ? "" : "not ")}a prime number");
+if (!int.TryParse (Console.ReadLine (), out int givenNumber)) Console.WriteLine ("Invlaid input");
+else {
+   if (givenNumber < 0) Console.WriteLine ("Negative Numbers cannot be prime.");
+   else if (givenNumber is 0 or 1) Console.WriteLine ($"{givenNumber} is neither prime nor composite.");
+   else if (givenNumber == 2) Console.WriteLine ("2 is a prime number");
+   else Console.WriteLine ($"{givenNumber} is {(IsPrime (givenNumber) ? "" : "not ")}a prime number");
+}
 
 bool IsPrime (int number) {
    for (int i = 1; i <= Math.Sqrt (number);) {
@@ -14,4 +16,4 @@ bool IsPrime (int number) {
       if (remainder == 0) return false;
    }
    return true;
-} 
+}
