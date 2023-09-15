a97e1b9fda6bcfb23e93b78add597a8d7868e59c
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-15 09:20:33 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -1,15 +1,11 @@
 ﻿// To print the factorial of a given number.
 
 Console.Write ("Enter a number to print its factorial: ");
-if (int.TryParse (Console.ReadLine (), out int input)) {
-   if (input < 0) Console.WriteLine("Factorial can be printed only for non-negative integers.");
-   else Console.WriteLine ($"Factorial of {input} is {Factorial (input)}.");
-} else Console.WriteLine ("Invalid Input.");
+if (!int.TryParse (Console.ReadLine (), out int input)) Console.WriteLine ("Invalid Input.");
+else Console.WriteLine ((input < 0) ? "Factorial can be printed only for non-negative integers." : $"Factorial of {input} is {Factorial (input)}.");
 
 int Factorial (int number) {
    int factorial = 1;
-   for (int i = 1; i <= number; i++) {
-      factorial *= i;
-   }
+   for (int i = 1; i <= number; i++) factorial *= i;
    return factorial;
-} 
+}
