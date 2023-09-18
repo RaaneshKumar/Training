a630dc5bb2460760ba35019ad3b6d7222782a805
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-18 14:22:43 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -2,28 +2,23 @@
 //Given an array of words, create a function which returns the longest abecedarian word. 
 //If no word in an array matches the criteria, return an empty string. 
 
-Console.WriteLine ("Enter no. words to check the longest abecedarian word: ");
+Console.Write ("Enter no. words to check the longest abecedarian word: ");
 bool input = int.TryParse (Console.ReadLine (), out int count);
-List<string> words = new List<string> ();
-string sorted = "";
-List<string> abecedarian = new List<string> ();
-if (input == true) {
+if (input) {
+   List<string> words = new ();
+   List<string> abecedarian = new ();
    for (int i = 0; i < count; i++) {
       Console.Write ("Enter a string: ");
-      string word = Console.ReadLine ();
-      words.Add (word);
+      words.Add (Console.ReadLine ());
    }
-   foreach (string each_word in words) {
-      List<char> letters = new List<char> ();
-      foreach (char item in each_word) letters.Add (item);
-      letters.Sort ();
-      foreach (char item in letters) sorted += item;
-      if (each_word == sorted) abecedarian.Add (sorted);
-      sorted = "";
+   foreach (string eachWord in words) {
+      char[] letters = eachWord.ToCharArray ();
+      Array.Sort (letters);
+      string sorted = new (letters);
+      if (eachWord == sorted) abecedarian.Add (sorted);
    }
    if (abecedarian.Count > 0) {
-      string result = abecedarian[0];
-      for (int i = 1; i < abecedarian.Count; i++) if (abecedarian[i].Length > result.Length) result = abecedarian[i];
-      Console.WriteLine (result);
-   }
+      string result = abecedarian.OrderByDescending (x => x.Length).First ();
+      Console.WriteLine ($"The longest abecedarian word is {result}");
+   } else Console.WriteLine ("No abecedarian words.");
 } else Console.WriteLine ("Invalid input.");
