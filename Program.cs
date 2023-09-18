224ca8a677bbf5a9deb0907fd817dd33368bd935
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-18 11:56:41 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿//An isogram is a word that has no duplicate letters.
 //Create a function that takes a string and returns either true or false depending on whether it's an "isogram". 
 
-Console.WriteLine ("Enter a string: ");
-string input = Console.ReadLine (); string output = "";
-if (isogram () == true) Console.WriteLine ("It is an isogram.");
-else Console.WriteLine ("Not an isogram.");
+Console.Write ("Enter a string: ");
+string input = Console.ReadLine () ?? "", output = "";
+if (input.Length > 0) Console.WriteLine ($"The given input is{(Isogram () ? "" : " not")} an isogram.");
+else Console.WriteLine ("Empty string");
 
-bool isogram () {
-   foreach (char item in input) {if (output.Contains (item) == false) output += item;}
+bool Isogram () {
+   foreach (char item in input) if (!output.Contains (item)) output += item;
    if (input == output) return true;
    return false;
 }
