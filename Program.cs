f5cdada00a85bb0a46affa814a6ae1a1624e40ae
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-18 14:41:22 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
 ﻿//Reduce a string of lowercase characters by deleting a pair of adjacent letters that match. 
 
-Console.WriteLine ("Enter a string: ");
-string user_input = Console.ReadLine ();
-user_input = user_input.ToLower();
-List<char> input = new List<char> () { };
-foreach (char item in user_input) input.Add (item);
+Console.Write ("Enter a string: ");
+string userInput = Console.ReadLine () ?? "".ToLower ();
+List<char> input = userInput.ToList ();
 for (int i = 1; i < input.Count; i++) {
    if (input[i] == input[i - 1]) {
-      input.RemoveAt (i); input.RemoveAt (i - 1);
+      input.RemoveAt (i);
+      input.RemoveAt (i - 1);
       i--;
    }
 }
-foreach (char item in input) Console.Write (item);
+Console.WriteLine (string.Join ("", input));
