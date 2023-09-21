1686e0697862586bfa417fbbe640bc0c319f3bc6
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-21 15:58:29 +0530

    Changes Done

--- Program.cs
+++ Program.cs
@@ -2,12 +2,16 @@
 //Create a function that takes a string and returns either true or false depending on whether it's an "isogram". 
 
 Console.Write ("Enter a string: ");
-string input = Console.ReadLine () ?? "", output = "";
-if (input.Length > 0) Console.WriteLine ($"The given input is{(Isogram () ? "" : " not")} an isogram.");
-else Console.WriteLine ("Empty string");
+string input = Console.ReadLine (), output = "";
+Console.WriteLine ((input.Length > 0) ? $"The given input is{(Isogram () ? "" : " not")} an isogram." : "Empty string.");
 
 bool Isogram () {
-   foreach (char item in input) if (!output.Contains (item)) output += item;
-   if (input == output) return true;
-   return false;
+   bool isogramCheck = true;
+   foreach (char item in input) {
+      if (output.Contains (item)) {
+         isogramCheck = false;
+         break;
+      } else output += item;
+   }
+   return isogramCheck;
 }
