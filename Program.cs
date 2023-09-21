6defbefede99d1d8c2d90e6d5c7f687034b7c914
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-21 14:11:43 +0530

    Changes Done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿//Reduce a string of lowercase characters by deleting a pair of adjacent letters that match. 
 
 Console.Write ("Enter a string: ");
-string userInput = Console.ReadLine () ?? "".ToLower ();
+string userInput = Console.ReadLine ().ToLower () ?? "";
 List<char> input = userInput.ToList ();
 for (int i = 1; i < input.Count; i++) {
    if (input[i] == input[i - 1]) {
