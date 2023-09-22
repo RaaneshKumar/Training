9db96cca0e0e4ffbbc070f7cb899fda17947225e
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-22 10:07:06 +0530

    Changed the code in the XML tag comments format

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Training
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2023.
+// Copyright (c) Metamation India.                                                
+// ---------------------------------------------------------------------------
+// Program.cs                                                                     
+// Program to swap indices from predefined array.
+// User enters two indices to swap and new array with swapped elements is displayed. 
+// Swapped elements and their indices are displayed in Green Foreground Color.
+// ---------------------------------------------------------------------------------------
+using static System.Console;
+namespace Training {
+   #region class Program ------------------------------------------------------------------------
+   internal class Program {
+      #region Method ----------------------------------------------
+      static void Main (string[] args) {
+         Console.WriteLine ("Hello, World!");
+      }
+      #endregion
+   }
+   #endregion
 }
