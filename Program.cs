59995e5385aa8cb9999cebd4717bae3a1ccb9398
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-09-22 11:27:28 +0530

    Changes Done

--- Program.cs
+++ Program.cs
@@ -1,40 +1,55 @@
-﻿namespace Training {
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2023.
+// Copyright (c) Metamation India.                                                
+// ---------------------------------------------------------------------------
+// Program.cs                                                                     
+// Program to swap indices from predefined array.
+// User enters two indices to swap and new array with swapped elements is displayed. 
+// Swapped elements and their indices are displayed in Green Foreground Color.
+// ---------------------------------------------------------------------------------------
+
+namespace Training {
+   #region class Program ------------------------------------------------------------------------
+   /// <summary> Armstrong Number </summary>
    internal class Program {
+      #region Method ----------------------------------------------
+      /// <summary>This method prints nth armstrong number when getting input from the command prompt and checks whether it is an armstrong number while executed in the file</summary>
+      /// <param name="args"></param>
       static void Main (string[] args) {
          if (args.Length > 0) {
             if (!int.TryParse (args[0], out int input)) Console.WriteLine ("Invalid Input.");
             else Console.WriteLine (NthArmstrong (input));
-         } else Console.WriteLine (Armstrong ());
+         } else {
+            Console.Write ("Enter a number to check whether it is an armstrong number or not: ");
+            if (!int.TryParse (Console.ReadLine (), out int input)) Console.WriteLine ("Invlaid Input.");
+            else Console.WriteLine ($"{input} is{(ArmstrongCheck (input) ? "" : " not")} an armstrong number.");
+         }
       }
+      /// <summary> This method gives the nth armstrong number where n is the input provide by the user </summary>
+      /// <param name="input"></param>
+      /// <returns> Returns the nth Armstrong number itself </returns>
       static int NthArmstrong (int input) {
          int count = 0;
          for (int i = 1; ; i++) {
-            double sum = 0;
-            int iDuplicate = i;
-            double length = i.ToString ().Length;
-            while (iDuplicate > 0) {
-               double digit = iDuplicate % 10;
-               sum += Math.Pow (digit, length);
-               iDuplicate /= 10;
-            }
-            if (sum == i) count++;
+            if (ArmstrongCheck (i)) count++;
             if (count == input) return i;
          }
       }
-      static string Armstrong () {
-         Console.Write ("Enter a number to check whether it is an armstrong number or not: ");
-         if (!int.TryParse (Console.ReadLine (), out int input)) return "Invlaid Input.";
-         else {
-            int inputCopy = input;
-            double sum = 0;
-            int length = input.ToString ().Length;
-            while (inputCopy > 0) {
-               double digit = inputCopy % 10;
-               sum += Math.Pow (digit, length);
-               inputCopy /= 10;
-            }
-            return $"{input} is{((input == sum) ? "" : " not")} an armstrong number.";
+      /// <summary>This method checks whether the given input is armstrong or not</summary>
+      /// <param name="input"></param>
+      /// <returns>Returns true when it is an armstrong number and false when it is not</returns>
+      static bool ArmstrongCheck (int input) {
+         int inputCopy = input;
+         double sum = 0;
+         int length = input.ToString ().Length;
+         while (inputCopy > 0) {
+            double digit = inputCopy % 10;
+            sum += Math.Pow (digit, length);
+            inputCopy /= 10;
          }
+         return input == sum;
       }
+      #endregion
    }
+   #endregion
 }
