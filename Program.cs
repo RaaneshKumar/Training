b796fc7ade8f3a3452b5b85c72a2b18b56203e93
RaaneshKumar <RaaneshKumar.Rajakumar@trumpf.com>
2023-09-29 14:26:09 +0530

    Changes Done

--- Program.cs
+++ Program.cs
@@ -15,17 +15,26 @@ namespace Training {
       /// <summary>This method gets the numbers from the user and displays the swapped numbers</summary>
       static void Main () {
          Console.WriteLine ("Enter two numbers to be swapped: ");
-         if (!double.TryParse (Console.ReadLine (), out double firstNumber)) Console.WriteLine ("Invalid Input.");
-         else {
-            if (!double.TryParse (Console.ReadLine (), out double secondNumber)) Console.WriteLine ("Invalid Input.");
-            else Console.WriteLine (Swap (firstNumber, secondNumber));
-         }
+         (double firstNumber, double secondNumber) = (ValidInputCheck (), ValidInputCheck ());
+         var swappedNumbers = Swap (firstNumber, secondNumber);
+         Console.WriteLine ($"Before Swapping\t: a = {firstNumber}, b = {secondNumber}\nAfter swapping\t: a = {swappedNumbers.Item1}, b = {swappedNumbers.Item2}");
+
       }
+
       /// <summary>This method swaps the two numbers</summary>
       /// <param name="a"></param>
       /// <param name="b"></param>
       /// <returns>Returns the swapped numbers in the form of a tuple</returns>
       static (double, double) Swap (double a, double b) => (b, a);
+
+      /// <summary>This method checks for valid input and converts it to a double if valid</summary>
+      /// <returns>Returns a double if input is valid</returns>
+      static double ValidInputCheck () {
+         for (int i = 1; ; i++) {
+            if (!double.TryParse (Console.ReadLine (), out double b)) Console.WriteLine ("Invalid Input.");
+            else return b;
+         }
+      }
       #endregion
    }
    #endregion
