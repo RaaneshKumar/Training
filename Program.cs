8d2522e3aecb3bea19fda80b545ed360ffc3c4cf
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-10-11 12:36:26 +0530

    Changes Done

--- Program.cs
+++ Program.cs
@@ -14,22 +14,20 @@ namespace Training {
       /// <summary>This method gets valid user input and displays the sorted and swapped array</summary>
       static void Main () {
          char[] letters;
-         for (; ; ) {
-            Console.Write ("Enter the characters to be sorted (as string): ");
-            letters = Console.ReadLine ().ToLower ().ToCharArray ();
-            if (letters.Any (x => !char.IsLetter (x))) {
-               Console.Write ("Only alphabets are allowed.\n");
-               continue;
-            } else break;
-         }
          string splCharInput;
          for (; ; ) {
+            Console.Write ("Enter the characters (only alphabets) to be sorted (as string): ");
+            letters = Console.ReadLine ().ToLower ().ToCharArray ();
             Console.Write ("Enter the special character to be swapped to the end: ");
             splCharInput = Console.ReadLine ();
-            if (splCharInput.Length != 1 || splCharInput.Any (x => !char.IsLetter (x))) {
+            if (letters.Length == 0
+                   || letters.Any (x => !char.IsLetter (x))
+                   || splCharInput.Length != 1
+                   || splCharInput.Any (x => !char.IsLetter (x))) {
                Console.Write ("Invalid Input.\n");
                continue;
-            } else break;
+            }
+            break;
          }
          char splChar = splCharInput[0];
          Console.Write ("Enter the order of sorting (D for descending, anything else ascending): ");
@@ -43,9 +41,9 @@ namespace Training {
       /// <param name="order"></param>
       /// <returns>Returns sorted and swapped array as string</returns>
       static string SortAndSwapSplChar (char[] letters, char splChar, char order = 'a') {
-         var sortedList = letters.Where (x => x != splChar).ToList ();
-         sortedList = order == 'd' ? sortedList.OrderDescending ().ToList () : sortedList.Order ().ToList ();
-         for (int i = 0; i < letters.Length - sortedList.Count; i++) sortedList.Add (splChar);
+         var sortedList = order == 'd' ? letters.Where (x => x != splChar).OrderDescending ().ToList ()
+                                       : letters.Where (x => x != splChar).Order ().ToList ();
+         sortedList.AddRange (Enumerable.Repeat (splChar, letters.Length - sortedList.Count));
          return String.Join ("", sortedList);
       }
       #endregion
