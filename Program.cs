24c99380c33688fd2a4c4667f94db785695d061c
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-10-12 16:03:50 +0530

    Changes Done

--- Program.cs
+++ Program.cs
@@ -11,24 +11,10 @@ namespace Training {
    /// <summary>Sort and Swap Array</summary>
    internal class Program {
       #region Method ----------------------------------------------
-      /// <summary>This method gets valid user input and displays the sorted and swapped array</summary>
+      /// <summary>This method displays the sorted and swapped array</summary>
       static void Main () {
-         char[] letters;
-         string splCharInput;
-         for (; ; ) {
-            Console.Write ("Enter the characters (only alphabets) to be sorted (as string): ");
-            letters = Console.ReadLine ().ToLower ().ToCharArray ();
-            Console.Write ("Enter the special character to be swapped to the end: ");
-            splCharInput = Console.ReadLine ();
-            if (letters.Length == 0
-                   || letters.Any (x => !char.IsLetter (x))
-                   || splCharInput.Length != 1
-                   || splCharInput.Any (x => !char.IsLetter (x))) {
-               Console.Write ("Invalid Input.\n");
-               continue;
-            }
-            break;
-         }
+         string letters = GetValidInput ("Enter the characters (only alphabets) to be sorted (as string): ");
+         string splCharInput = GetValidInput ("Enter the special character to be swapped to the end: ", false);
          char splChar = splCharInput[0];
          Console.Write ("Enter the order of sorting (D for descending, anything else ascending): ");
          Console.Write ($"After Sorting and Swapping: {(Console.ReadLine () is "D" or "d" ?
@@ -40,11 +26,25 @@ namespace Training {
       /// <param name="splChar"></param>
       /// <param name="order"></param>
       /// <returns>Returns sorted and swapped array as string</returns>
-      static string SortAndSwapSplChar (char[] letters, char splChar, char order = 'a') {
+      static string SortAndSwapSplChar (string letters, char splChar, char order = 'a') {
          var sortedList = order == 'd' ? letters.Where (x => x != splChar).OrderDescending ().ToList ()
                                        : letters.Where (x => x != splChar).Order ().ToList ();
          sortedList.AddRange (Enumerable.Repeat (splChar, letters.Length - sortedList.Count));
          return String.Join ("", sortedList);
+      }
+
+      /// <summary>This method gets the valid input from the user</summary>
+      /// <param name="command"></param>
+      /// <param name="isLenAlso"></param>
+      /// <returns>Reurns the valid Input</returns>
+      static string GetValidInput (string command, bool isLenAlso = true) {
+         for (; ; ) {
+            Console.Write (command);
+            string input = Console.ReadLine ().Trim ().ToLower ();
+            if (String.IsNullOrEmpty (input) || input.Any (x => !char.IsLetter (x))) { Console.WriteLine ("Invalid Input."); continue; }
+            if (!isLenAlso && input.Length != 1) { Console.WriteLine ("Invalid Input."); continue; }
+            return input;
+         }
       }
       #endregion
    }
