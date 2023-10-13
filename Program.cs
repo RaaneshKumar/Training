f9686e09623ffa4e4a66b516b27ea679768658b2
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-10-13 17:00:10 +0530

    Changes Done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// ---------------------------------------------------------------------------------------
 // Training ~ A training program for new joinees at Metamation, Batch- July 2023.
 // Copyright (c) Metamation India.                                                
-// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------
 // Program.cs                                                                     
 // Program to print a chess board using unicode values
 // ---------------------------------------------------------------------------------------
@@ -9,36 +9,59 @@ using System.Text;
 using static System.Console;
 
 namespace Training {
-   #region class Program ------------------------------------------------------------------------
+   #region class Program --------------------------------------------------------------------------
    /// <summary>Chess Board</summary>
    internal class Program {
-      #region Method ----------------------------------------------
+      #region Method ------------------------------------------------
       /// <summary>This method prints the chess board with all pieces</summary>
       static void Main () {
          OutputEncoding = Encoding.UTF8;
-         string blRook = "\u265C", blKnight = "\u265E", blBishop = "\u265D", vLine = "\u2502",
-                whRook = "\u2656", whKnight = "\u2658", whBishop = "\u2657", hLine = "\u2500";
+         string vLine = "\u2502", hLine = "\u2500";
+
          // Black : 265F - pawn, 265C - rook, 265E - knight, 265D - bishop, 265B - queen, 265A - king
          // White : 2659 - pawn, 2656 - rook, 2658 - knight, 2657 - bishop, 2655 - queen, 2654 - king
-
          for (int i = 0; i < 10; i++) {
             switch (i) {
-               case 0: WriteLine ("\u250C" + string.Concat (Enumerable.Repeat ($"{hLine}{hLine}{hLine}\u252C", 7)) + $"{hLine}{hLine}{hLine}\u2510"); continue;
-               case 1: WriteLine ("{0} {1} {0} {2} {0} {3} {0} \u265B {0} \u265A {0} {3} {0} {2} {0} {1} {0}", vLine, blRook, blKnight, blBishop); continue;
-               case 2 or 4 or 7: WriteLine ("\u251C" + string.Concat (Enumerable.Repeat ($"{hLine}{hLine}{hLine}\u253C", 7)) + $"{hLine}{hLine}{hLine}\u2524"); continue;
-               case 3: WriteLine (string.Concat (Enumerable.Repeat ($"{vLine} \u265F ", 8)) + $"{vLine}"); continue;
+               case 0: PrintHrzntLines (hLine, "\u250C", "\u252C", "\u2510"); continue;
+               case 1: PrintPieces (vLine, "\u265C", "\u265E", "\u265D", "\u265B", "\u265A"); continue;
+               case 2 or 4 or 7: PrintHrzntLines (hLine, "\u251C", "\u253C", "\u2524"); continue;
+               case 3: PrintPawnsOrSpaces (vLine, "\u265F"); continue;
                case 5:
                   for (int j = 0; j < 4; j++) {
-                     WriteLine (string.Concat (Enumerable.Repeat ($"{vLine}   ", 8)) + $"{vLine}");
-                     WriteLine ("\u251C" + string.Concat (Enumerable.Repeat ($"{hLine}{hLine}{hLine}\u253C", 7)) + $"{hLine}{hLine}{hLine}\u2524");
+                     PrintPawnsOrSpaces (vLine, " ");
+                     PrintHrzntLines (hLine, "\u251C", "\u253C", "\u2524");
                   }
                   continue;
-               case 6: WriteLine (string.Concat (Enumerable.Repeat ($"{vLine} \u2659 ", 8)) + $"{vLine}"); continue;
-               case 8: WriteLine ("{0} {1} {0} {2} {0} {3} {0} \u2655 {0} \u2654 {0} {3} {0} {2} {0} {1} {0}", vLine, whRook, whKnight, whBishop); continue;
-               case 9: WriteLine ("\u2514" + string.Concat (Enumerable.Repeat ($"{hLine}{hLine}{hLine}\u2534", 7)) + $"{hLine}{hLine}{hLine}\u2518"); continue;
+               case 6: PrintPawnsOrSpaces (vLine, "\u2659"); continue;
+               case 8: PrintPieces (vLine, "\u2656", "\u2658", "\u2657", "\u2655", "\u2654"); continue;
+               case 9: PrintHrzntLines (hLine, "\u2514", "\u2534", "\u2518"); continue;
             }
          }
       }
+
+      /// <summary>Prints the horizontal lines</summary>
+      /// <param name="hLine">Horizontal Line</param>
+      /// <param name="start">Left joint</param>
+      /// <param name="mid">Mid Joint</param>
+      /// <param name="end">Right Joint</param>
+      static void PrintHrzntLines (string hLine, string start, string mid, string end)
+         => WriteLine ($"{start}" + string.Concat (Enumerable.Repeat ($"{hLine}{hLine}{hLine}{mid}", 7)) + $"{hLine}{hLine}{hLine}{end}");
+
+      /// <summary>Prints the black and white pieces</summary>
+      /// <param name="vLine">Vertical Line</param>
+      /// <param name="rook"></param>
+      /// <param name="knight"></param>
+      /// <param name="bishop"></param>
+      /// <param name="queen"></param>
+      /// <param name="king"></param>
+      static void PrintPieces (string vLine, string rook, string knight, string bishop, string queen, string king)
+         => WriteLine ("{0} {1} {0} {2} {0} {3} {0} {4} {0} {5} {0} {3} {0} {2} {0} {1} {0}", vLine, rook, knight, bishop, queen, king);
+
+      /// <summary>Prints black and white pawns or empty spaces</summary>
+      /// <param name="vLine">Vertical Line</param>
+      /// <param name="pawn"></param>
+      static void PrintPawnsOrSpaces (string vLine, string pawn)
+         => WriteLine (string.Concat (Enumerable.Repeat ($"{vLine} {pawn} ", 8)) + $"{vLine}");
       #endregion
    }
    #endregion
