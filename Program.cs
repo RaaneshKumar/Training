2c40e75a40903a97995945eca09d0eaa01d3777c
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-10-16 12:41:03 +0530

    Made changes

--- Program.cs
+++ Program.cs
@@ -1,96 +1,57 @@
-﻿Console.Write ("Enter a number: ");
-if (int.TryParse (Console.ReadLine (), out int num)) {
-   Console.Write ("If you want to convert into words, enter w. If you want to convert to roman numerals, enter r. (Condition: Limit to convert to words is 10 crores. Limit to convert to roman numerals is 3999. Enter your choice: ");
-   string choice = Console.ReadLine ();
-   if (choice == "w") {
-      if (num > 100000000) Console.WriteLine ("Enter value within the limit."); else Console.WriteLine (ToWords (num));
-   } else if (choice == "r") {
-      if (num > 3999) Console.WriteLine ("Enter value within the limit."); else Console.WriteLine (ToRomanNumerals (num));
-   } else Console.WriteLine ("Invlaid Input.");
-} else Console.WriteLine ("Invalid Input");
-
-// To print the given number in words
-// For example: 376 must be printed as three hundred and seventy six
-// Code is written for user input upto 10 crores
-
-string ToWords (int num) {
-   string numberInWords = "";
+﻿Console.WriteLine ("Enter a number: \n" +
+                   "(Condition: Limit to convert to words is 10 crores. Limit to convert to roman numerals is 3999.)");
+if (!int.TryParse (Console.ReadLine (), out int num)) {
+   Console.WriteLine ("Invalid Input");
+   return;
+}
+Console.Write ("If you want to convert into words, enter w. If you want to convert to roman numerals, enter r.");
+string choice = Console.ReadLine ();
+if (choice == "w") Console.WriteLine (num > 100000000 ? "Enter value within the limit." : GetNumInWords (num));
+//else if (choice == "r") Console.WriteLine (num > 3999 ? "Enter value within the limit." : ToRomanNumerals (num));
+else Console.WriteLine ("Invlaid Input.");
+GetRomanNum (num);
+string GetNumInWords (int num) {
+   string numInWrds = "";
+   Dictionary<int, int> divisor = new () { { 1, 1 }, { 2, 1 }, { 3, 100 }, { 4, 1000 }, { 5, 1000 }, { 6, 100000 }, { 7, 100000 }, { 8, 10000000 }, { 9, 10000000 }};
    while (num > 0) {
-      if (num >= 10000000) {
-         int quotient = num / 10000000; int remainder = num % 10000000;
-         Console.Write ((OneToNinetyNine (quotient)) + " crores ");
-         num = remainder;
-      }
-      if (num >= 100000) {
-         int quotient = num / 100000; int remainder = num % 100000;
-         Console.Write ((OneToNinetyNine (quotient)) + " lakhs ");
-         num = remainder;
-      }
-      if (num >= 1000) {
-         int quotient = num / 1000; int remainder = num % 1000;
-         Console.Write ((OneToNinetyNine (quotient)) + " thousand ");
-         num = remainder;
-      }
-      if (num >= 100) {
-         int quotient = num / 100; int remainder = num % 100;
-         Console.Write ((OneToNinetyNine (quotient)) + " hundred and ");
-         num = remainder;
-      }
-      if (num >= 0) Console.Write ((OneToNinetyNine (num)));
-      Console.WriteLine (numberInWords);
-      break;
+      (string words, num) = GetWords (num, divisor[num.ToString ().Length]);
+      numInWrds += words;
    }
-
-
-   string OneToNinetyNine (int num1) {
-      string quotientstring = "";
-      if (num1 >= 20) {
-         int quotient1 = num1 / 10; int remainder1 = num1 % 10;
-         Dictionary<int,string> twentyToNinety = new Dictionary<int,string> () { { 2, "twenty "}, { 3, "thirty " }, { 4, "forty " }, { 5, "fifty " }, { 6, "sixty " }, { 7, "seventy " }, { 8, "eighty " }, { 9, "ninety " } };
-         quotientstring = twentyToNinety[quotient1];
-         num1 = remainder1;
-      }
-      Dictionary<int, string> oneToNineteen = new Dictionary<int, string> () { { 1, "one" }, { 2, "two" }, { 3, "three" }, { 4, "four" }, { 5, "five" }, { 6, "six" }, { 7, "seven" }, { 8, "eight" }, { 9, "nine" }, { 10, "ten" }, { 11, "eleven" }, { 12, "twelve" }, { 13, "thirteen" }, { 14, "fourteen" }, { 15, "fifteen" }, { 16, "sixteen" }, { 17, "seventeen" }, { 18, "eighteen" }, { 19, "nineteen" } };
-      quotientstring += oneToNineteen[num1];
-      return quotientstring;
-   }
-   return numberInWords;
+   return numInWrds;
 }
 
-// Convert a given number to its corresponding roman numerals
-// Code is written for User input less than 4000
-
-string ToRomanNumerals (int input) {
-   string numberInRoman = "";
-   while (input > 0) {
-      if (input > 1000) {
-         int quotient = input / 1000; int remainder = input % 1000;
-         numberInRoman = string.Concat (Enumerable.Repeat ("M", quotient));
-         input = remainder;
-      }
-      if (input > 100) {
-         int quotient = input / 100; int remainder = input % 100;
-         if (quotient == 9) numberInRoman += "CM";
-         if (quotient > 5 && quotient < 9) numberInRoman += "D" + string.Concat (Enumerable.Repeat ("C", quotient - 5));
-         if (quotient == 5) numberInRoman += "D";
-         if (quotient == 4) numberInRoman += "CD";
-         if (quotient >= 1 && quotient < 4) numberInRoman += string.Concat(Enumerable.Repeat("C", quotient));
-         input = remainder;
-      }
-      if (input > 10) {
-         int quotient = input / 10; int remainder = input % 10;
-         if (quotient == 9) numberInRoman += "XC";
-         if (quotient > 5 && quotient < 9) numberInRoman += "L" + string.Concat(Enumerable.Repeat("X", quotient - 5));
-         if (quotient == 5) numberInRoman += "L";
-         if (quotient == 4) numberInRoman += "XL";
-         if (quotient >= 1 && quotient < 4) numberInRoman += string.Concat(Enumerable.Repeat("X", quotient));
-         input = remainder;
-      }
-      if (input >= 1) {
-         Dictionary<int,string> romanNumbers = new Dictionary<int, string> () { {1, "I"}, { 2, "II" }, { 3, "III" }, { 4, "IV" }, { 5, "V" }, { 6, "VI" }, { 7, "VII" }, { 8, "VIII" }, { 9, "IX" }, { 10, "X" } };
-         numberInRoman += romanNumbers[input];
-      }
-      break;
+(string wrds, int rem) GetWords (int num, int div = 1) {
+   int quo, rem, tempQuo, tempRem;
+   string wrds = "";
+   Dictionary<int, string> digits = new () { { 1, "one" }, { 2, "two" }, { 3, "three" }, { 4, "four" }, { 5, "five" },
+                                             { 6, "six" }, { 7, "seven" }, { 8, "eight" }, { 9, "nine" }, { 10, "ten" },
+                                             { 11, "eleven" }, { 12, "twelve" }, { 13, "thirteen" }, { 14, "fourteen" }, { 15, "fifteen" },
+                                             { 16, "sixteen" }, { 17, "seventeen" }, { 18, "eighteen" }, { 19, "nineteen" } };
+   Dictionary<int, string> twentyToNinety = new () { { 2, "twenty "}, { 3, "thirty " }, { 4, "forty " }, { 5, "fifty " },
+                                                     { 6, "sixty " }, { 7, "seventy " }, { 8, "eighty " }, { 9, "ninety " }};
+   Dictionary<int, string> divisor = new () { { 10000000, "crore(s)" }, { 100000, "lakh(s)" }, { 1000, "thousand" }, { 100, "hundred and" } };
+   quo = num / div; rem = num % div;
+   if (quo >= 20) {
+      tempQuo = quo / 10; tempRem = quo % 10;
+      wrds = twentyToNinety[tempQuo];
+      quo = tempRem;
    }
-   return numberInRoman; 
+   if (quo != 0) wrds += digits[quo] + $" {(div == 1 ? "" : divisor[div])} ";
+   return (wrds, rem);
+}
+
+
+
+
+void GetRomanNum (int num) {
+   string a = "";
+   //Dictionary<int, char> romNum = new () { { 1, 'I' }, { 5, 'V' }, { 10, 'X' }, { 50, 'L' }, { 100, 'C' }, { 500, 'D' }, { 1000, 'M' } };
+   List<int> number = new () { 1, 5, 10, 50, 100, 500, 1000, 5000 };
+   List<char> romNum = new () { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+   while (num > 0) {
+      int b = number.IndexOf (number.Where (x => num <= x).ToList ()[0]) - 1;
+      a += romNum[b];
+      num -= number[b];
+   }
+   Console.WriteLine (a);
 }
