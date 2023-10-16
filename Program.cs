5fc6c17a2faae0abff29fa2454197b5e06026e03
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-10-16 14:15:38 +0530

    Changes Done

--- Program.cs
+++ Program.cs
@@ -16,25 +16,24 @@ namespace Training {
       /// <summary>This method prints the chess board with all pieces</summary>
       static void Main () {
          OutputEncoding = Encoding.UTF8;
-         string vLine = "\u2502", hLine = "\u2500";
 
          // Black : 265F - pawn, 265C - rook, 265E - knight, 265D - bishop, 265B - queen, 265A - king
          // White : 2659 - pawn, 2656 - rook, 2658 - knight, 2657 - bishop, 2655 - queen, 2654 - king
          for (int i = 0; i < 10; i++) {
             switch (i) {
-               case 0: PrintHrzntLines (hLine, "\u250C", "\u252C", "\u2510"); continue;
-               case 1: PrintPieces (vLine, "\u265C", "\u265E", "\u265D", "\u265B", "\u265A"); continue;
-               case 2 or 4 or 7: PrintHrzntLines (hLine, "\u251C", "\u253C", "\u2524"); continue;
-               case 3: PrintPawnsOrSpaces (vLine, "\u265F"); continue;
+               case 0: PrintHLines (hLine, "\u250C", "\u252C", "\u2510"); break;
+               case 1: PrintPieces (vLine, "\u265C", "\u265E", "\u265D", "\u265B", "\u265A"); break;
+               case 2 or 4 or 7: PrintHLines (hLine, "\u251C", "\u253C", "\u2524"); break;
+               case 3: PrintPawnsOrSpaces (vLine, "\u265F"); break;
                case 5:
                   for (int j = 0; j < 4; j++) {
                      PrintPawnsOrSpaces (vLine, " ");
-                     PrintHrzntLines (hLine, "\u251C", "\u253C", "\u2524");
+                     PrintHLines (hLine, "\u251C", "\u253C", "\u2524");
                   }
-                  continue;
-               case 6: PrintPawnsOrSpaces (vLine, "\u2659"); continue;
-               case 8: PrintPieces (vLine, "\u2656", "\u2658", "\u2657", "\u2655", "\u2654"); continue;
-               case 9: PrintHrzntLines (hLine, "\u2514", "\u2534", "\u2518"); continue;
+                  break;
+               case 6: PrintPawnsOrSpaces (vLine, "\u2659"); break;
+               case 8: PrintPieces (vLine, "\u2656", "\u2658", "\u2657", "\u2655", "\u2654"); break;
+               case 9: PrintHLines (hLine, "\u2514", "\u2534", "\u2518"); break;
             }
          }
       }
@@ -44,8 +43,9 @@ namespace Training {
       /// <param name="start">Left joint</param>
       /// <param name="mid">Mid Joint</param>
       /// <param name="end">Right Joint</param>
-      static void PrintHrzntLines (string hLine, string start, string mid, string end)
-         => WriteLine ($"{start}" + string.Concat (Enumerable.Repeat ($"{hLine}{hLine}{hLine}{mid}", 7)) + $"{hLine}{hLine}{hLine}{end}");
+      static void PrintHLines (string hLine, string start, string mid, string end)
+         => WriteLine ($"{start}" + string.Concat (Enumerable.Repeat ($"{hLine}{hLine}{hLine}{mid}", 7))
+                                                                    + $"{hLine}{hLine}{hLine}{end}");
 
       /// <summary>Prints the black and white pieces</summary>
       /// <param name="vLine">Vertical Line</param>
@@ -55,13 +55,18 @@ namespace Training {
       /// <param name="queen"></param>
       /// <param name="king"></param>
       static void PrintPieces (string vLine, string rook, string knight, string bishop, string queen, string king)
-         => WriteLine ("{0} {1} {0} {2} {0} {3} {0} {4} {0} {5} {0} {3} {0} {2} {0} {1} {0}", vLine, rook, knight, bishop, queen, king);
+         => WriteLine ("{0} {1} {0} {2} {0} {3} {0} {4} {0} {5} {0} {3} {0} {2} {0} {1} {0}",
+                                                   vLine, rook, knight, bishop, queen, king);
 
       /// <summary>Prints black and white pawns or empty spaces</summary>
       /// <param name="vLine">Vertical Line</param>
       /// <param name="pawn"></param>
       static void PrintPawnsOrSpaces (string vLine, string pawn)
          => WriteLine (string.Concat (Enumerable.Repeat ($"{vLine} {pawn} ", 8)) + $"{vLine}");
+      #endregion
+
+      #region Private -----------------------------------------------
+      static string vLine = "\u2502", hLine = "\u2500";
       #endregion
    }
    #endregion
