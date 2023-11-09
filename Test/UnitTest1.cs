b3ce782981b93481daf60d7b177c810e075a272a
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-11-09 10:49:47 +0530

    Made changes

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,10 +1,8 @@
 namespace Test;
 
 [TestClass]
-public class UnitTest1
-{
-    [TestMethod]
-    public void TestMethod1()
-    {
-    }
+public class UnitTest1 {
+   [TestMethod]
+   public void TestMethod1 () {
+   }
 }
