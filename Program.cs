40616ded4b1154f73418d9e7ef36cf300a206523
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-11-09 16:04:07 +0530

    Completed T28

--- Program.cs
+++ Program.cs
@@ -3,17 +3,65 @@
 // Copyright (c) Metamation India.                                                
 // ---------------------------------------------------------------------------------------
 // Program.cs                                                                     
-// 
-// 
-// 
+// Program to create a custom TQueue<T> class.
+// Class is created with arrays as the underlying data structure.
+// Must start with an initial capacity of 4 and double its capacity when needed.
+// Throw exceptions when necessary. 
 // ---------------------------------------------------------------------------------------
 namespace Training {
    #region class Program --------------------------------------------------------------------------
    internal class Program {
       #region Method ------------------------------------------------
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+      static void Main () { }
+      #endregion
+   }
+
+   /// <summary>Custom TQueue<T> class</summary>
+   public class TQueue<T> {
+      #region Property ----------------------------------------------
+      ///<summary>Gets the count of the TQueue</summary>
+      public int Count => mCount;
+
+      /// <summary>Gets the size of the TQueue</summary>
+      public int Capacity => mQueue.Length;
+
+      /// <summary>Checks whether TQueue is empty</summary>
+      public bool IsEmpty => Count == 0;
+      #endregion
+
+      #region Method ------------------------------------------------
+      /// <summary>Adds an element to the end of the queue</summary>
+      /// <param name="a">The element to be added</param>
+      public void Enqueue (T a) {
+         if (Count == Capacity) Array.Resize (ref mQueue, Capacity * 2);
+         mQueue[mCount++] = a;
       }
+
+      /// <summary>Removes and returns the first element from the queue</summary>
+      public T Dequeue () {
+         InvalidOperationExceptionCheck ();
+         (var firstItem, mQueue[mDequeueCount]) = (mQueue[mDequeueCount], default);
+         mDequeueCount++; mCount--;
+         return firstItem;
+      }
+
+      /// <summary>Returns the first element from the queue</summary>
+      public T Peek () {
+         InvalidOperationExceptionCheck ();
+         return mQueue[mDequeueCount];
+      }
+
+      /// <summary>Throws exception when attempting to dequeue or peek an empty queue</summary>
+      /// <exception cref="InvalidOperationException">Throwed when Dequeue/Peek is called on empty list</exception>
+      void InvalidOperationExceptionCheck () {
+         if (IsEmpty) throw new InvalidOperationException ();
+      }
+      #endregion
+
+      #region Private -----------------------------------------------
+      T[] mQueue = new T[4];
+      int mCount = 0;
+      int mDequeueCount = 0;
       #endregion
    }
    #endregion
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,8 +1,39 @@
-namespace Test;
+using Training;
 
-[TestClass]
-public class UnitTest1 {
-   [TestMethod]
-   public void TestMethod1 () {
+namespace Test {
+   [TestClass]
+   public class UnitTest1 {
+      [TestMethod]
+      public void EnqueueTest () {
+         myQueue.Enqueue (1);
+         myQueue.Enqueue (2);
+         queue.Enqueue (1);
+         queue.Enqueue (2);
+         Assert.AreEqual (queue.Count, myQueue.Count);
+      }
+
+      [TestMethod]
+      public void DequeueTest () {
+         Assert.ThrowsException<InvalidOperationException> (() => myQueue.Dequeue ());
+         myQueue.Enqueue (1);
+         myQueue.Enqueue (2);
+         queue.Enqueue (1);
+         queue.Enqueue (2);
+         Assert.AreEqual (queue.Dequeue (), myQueue.Dequeue ());
+         Assert.AreEqual (queue.Count, myQueue.Count);
+      }
+
+      [TestMethod]
+      public void PeekTest () {
+         Assert.ThrowsException<InvalidOperationException> (() => myQueue.Peek ());
+         myQueue.Enqueue (1);
+         myQueue.Enqueue (2);
+         queue.Enqueue (1);
+         queue.Enqueue (2);
+         Assert.AreEqual (queue.Peek (), myQueue.Peek ());
+      }
+
+      TQueue<int> myQueue = new ();
+      Queue<int> queue = new ();
    }
 }
