2e23570d48f9dc3a64382549b3d79eacba5e4904
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-11-28 17:08:35 +0530

    Completed A7

--- Program.cs
+++ Program.cs
@@ -3,16 +3,91 @@
 // Copyright (c) Metamation India.                                                
 // ---------------------------------------------------------------------------------------
 // Program.cs                                                                     
-// 
-// 
-// 
+// Program to implement double.Parse method that takes a string and returns a double.
 // ---------------------------------------------------------------------------------------
+
 namespace Training {
    #region class Program --------------------------------------------------------------------------
-   internal class Program {
+   /// <summary>Double.Parse</summary>
+   public class DoubleParse {
       #region Method ------------------------------------------------
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+      /// <summary>Gets the user input and prints the desired output</summary>
+      static void Main () {
+         Console.Write ("Enter a value to be converted into double: ");
+         string input = Console.ReadLine ().ToLower ();
+         Console.WriteLine (IsValid (input) ? Parse (input) : "Invalid Input");
+      }
+
+      /// <summary>Checks if the user input is valid</summary>
+      /// <param name="s">user input</param>
+      /// <returns>Returns true if the input is valid</returns>
+      public static bool IsValid (string s) {
+         if (s.Any (x => x != '+' && x != '-'                 // Checks if all are valid characters.
+                      && x != '.' && x != 'e'
+                      && !char.IsDigit (x))) return false;
+         if (s.All (x => !char.IsDigit (x))) return false;    // Checks if atleast one digit is present.
+         if (s[0] == 'e' || s[^1] is 'e') return false;       // Checks if first and last characters are valid.
+         if ((s.Where (x => x == '.').Count () > 1) ||        // Checks if multiple decimal points are present.
+             (s.Where (x => x == 'e').Count () > 1)) return false; // Checks if multiple E's are present.
+         if (!ExpressionCheck ('+') || !ExpressionCheck ('-')) return false;
+         return true;
+
+         /// <summary>Checks if the '+' or '-' in user input is unary</summary>
+         /// <param name="a">char '+' or '-'</param>
+         bool ExpressionCheck (char a) {
+            switch (s.Where (x => x == a).Count ()) {
+               case 1:
+                  if (s[0] != a && s[s.IndexOf ('e') + 1] != a) return false;
+                  break;
+               case 2:
+                  if (ContainsE (s) && (s[0] != a || s[s.IndexOf ('e') + 1] != a)) return false;
+                  break;
+               case > 2: return false;
+            }
+            return true;
+         }
+      }
+
+      /// <summary>Coverts the string to a double based on the type of input</summary>
+      /// <returns>Returns the converted double value</returns>
+      public static double Parse (string s) {
+         if (ContainsE (s)) return GetSolvedE (s);
+         if (!ContainsPoint (s)) return int.Parse (s); // Means an integer
+         return GetSolvedPoint (s);
+      }
+
+      /// <summary>Checks if the input has 'e' in it</summary>
+      /// <returns></returns>
+      static bool ContainsE (string s) => s.Any (x => x == 'e');
+
+      /// <summary>Converts the string to double if E is present</summary>
+      /// <returns>Returns the converted double value</returns>
+      static double GetSolvedE (string s) {
+         double firstPart, secPart;
+         string[] parts = s.Split ('e');
+
+         firstPart = ContainsPoint (parts[0]) ? GetSolvedPoint (parts[0]) : int.Parse (parts[0]);
+         secPart = ContainsPoint (parts[1]) ? GetSolvedPoint (parts[1]) : int.Parse (parts[1]);
+         return firstPart * Math.Pow (10, secPart);
+      }
+
+      /// <summary>Checks if the input has a decimal point in it</summary>
+      /// <returns>Returns true if decimal point is present</returns>
+      static bool ContainsPoint (string s) => s.Any (x => x == '.');
+
+      /// <summary>Converts the string to double if decimal pint is present.</summary>
+      /// <returns>Returns the converted double value</returns>
+      static double GetSolvedPoint (string s) {
+         string[] parts = s.Split ('.');
+         int integral = 0, fractional = 0;
+
+         if (s[0] == '.' ||                       // Checks for values like .2 and -.2
+             (parts[0] == "-" && s[1] == '.')) fractional = int.Parse (parts[1]);
+         else if (s[^1] == '.') integral = int.Parse (parts[0]); // Checks for values ending with a decimal point.
+         else { integral = int.Parse (parts[0]); fractional = int.Parse (parts[1]); }
+
+         double parsedDouble = Math.Abs (integral) + (fractional / Math.Pow (10, parts[1].Length));
+         return s[0] == '-' ? -parsedDouble : parsedDouble;
       }
       #endregion
    }
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,8 +1,37 @@
-namespace Test;
+using Training;
 
-[TestClass]
-public class UnitTest1 {
-   [TestMethod]
-   public void TestMethod1 () {
+namespace Test {
+   [TestClass]
+   public class UnitTest1 {
+      [TestMethod]
+      public void DoubleParseValidTest () {
+         Assert.AreEqual (double.Parse ("12"), DoubleParse.Parse ("12"));
+         Assert.AreEqual (double.Parse ("12.5"), DoubleParse.Parse ("12.5"));
+         Assert.AreEqual (double.Parse (".3"), DoubleParse.Parse (".3"));
+         Assert.AreEqual (double.Parse ("-12.0"), DoubleParse.Parse ("-12.0"));
+         Assert.AreEqual (double.Parse ("-.2"), DoubleParse.Parse ("-.2"));
+         Assert.AreEqual (double.Parse ("1e13"), DoubleParse.Parse ("1e13"));
+         Assert.AreEqual (double.Parse ("3.15e-3"), DoubleParse.Parse ("3.15e-3"));
+         Assert.AreEqual (double.Parse ("3.4e+4"), DoubleParse.Parse ("3.4e+4"));
+         Assert.AreEqual (double.Parse ("3.e2"), DoubleParse.Parse ("3.e2"));
+      }
+
+      [TestMethod]
+      public void DoubleParseInValidTest () {
+         Assert.IsFalse (DoubleParse.IsValid ("123a"));
+         Assert.IsFalse (DoubleParse.IsValid ("456.a"));
+         Assert.IsFalse (DoubleParse.IsValid ("-789.a"));
+         Assert.IsFalse (DoubleParse.IsValid ("e123"));
+         Assert.IsFalse (DoubleParse.IsValid ("456e"));
+         Assert.IsFalse (DoubleParse.IsValid ("789.e"));
+         Assert.IsFalse (DoubleParse.IsValid ("123+"));
+         Assert.IsFalse (DoubleParse.IsValid ("456-"));
+         Assert.IsFalse (DoubleParse.IsValid ("12.34.5"));
+         Assert.IsFalse (DoubleParse.IsValid ("12e3e2"));
+         Assert.IsFalse (DoubleParse.IsValid ("12+12"));
+         Assert.IsFalse (DoubleParse.IsValid ("12-32"));
+         Assert.IsFalse (DoubleParse.IsValid ("+1+2e+2"));
+         Assert.IsFalse (DoubleParse.IsValid ("+e."));
+      }
    }
 }
