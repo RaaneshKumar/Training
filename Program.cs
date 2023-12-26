91e8759000232533204a7ed9b33a1e56f9feb1eb
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2023-12-26 12:24:14 +0530

    A14.1 : Anagrams

--- Program.cs
+++ Program.cs
@@ -3,16 +3,27 @@
 // Copyright (c) Metamation India.                                                
 // ---------------------------------------------------------------------------------------
 // Program.cs                                                                     
-// 
-// 
-// 
+// Find all the anagrams and sort them based on the anagrams count from the words in the word list.
 // ---------------------------------------------------------------------------------------
 namespace Training {
    #region class Program --------------------------------------------------------------------------
+   /// <summary>Anagrams</summary>
    internal class Program {
       #region Method ------------------------------------------------
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+      static void Main () => Anagrams ();
+
+      /// <summary>This method finds all anagrams and 
+      /// sorts them based on the anagrams count from the words</summary>
+      static void Anagrams () {
+         var file = File.ReadLines ("C:/etc/words.txt");
+
+         var anagrams = file.GroupBy (x => String.Join ("", x.Order ())) // Groups by anagrams
+                            .Where (x => x.Count () > 1) // Removes all words having no anagrams
+                            //.Select (x => x.Order ())
+                            .OrderByDescending (x => x.Count());
+
+         foreach (var item in anagrams)
+            Console.WriteLine (item.Count() + " " + String.Join (" ", item));
       }
       #endregion
    }
