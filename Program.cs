42ee8ba3c669d11fdc79b836287f99b9b2da7ffb
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2024-01-24 12:30:49 +0530

    Completed Test02

--- Program.cs
+++ Program.cs
@@ -3,18 +3,49 @@
 // Copyright (c) Metamation India.                                                
 // ---------------------------------------------------------------------------------------
 // Program.cs                                                                     
-// 
-// 
-// 
+// Complex Number Class Implementation.
+// Overload + and - operators.
+// Implement property Norm to find the magnitude
 // ---------------------------------------------------------------------------------------
+using System.ComponentModel;
+
 namespace Training {
    #region class Program --------------------------------------------------------------------------
    internal class Program {
       #region Method ------------------------------------------------
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+      static void Main () { }
       #endregion
+   }
+
+   public class ComplexNumber {
+      public ComplexNumber (int real, int img) {
+         mReal = real; mImg = img;
+      }
+      int mReal, mImg;
+
+      public int RealPart {
+         get { return mReal; }
+         set { mReal = value; }
+      }
+
+      public int ImgPart {
+         get { return mImg; }
+         set { mImg = value; }
+      }
+
+      public double Norm => Math.Sqrt ((mReal * mReal) + (mImg * mImg));
+
+      public static ComplexNumber operator + (ComplexNumber m1, ComplexNumber m2) => Add (m1, m2);
+
+      public static ComplexNumber operator - (ComplexNumber m1, ComplexNumber m2) => Subtract (m1, m2);
+
+      static ComplexNumber Add (ComplexNumber m1, ComplexNumber m2)
+         => new (m1.RealPart + m2.RealPart, m1.ImgPart + m2.ImgPart);
+
+      static ComplexNumber Subtract (ComplexNumber m1, ComplexNumber m2)
+         => new (m1.RealPart - m2.RealPart, m1.ImgPart - m2.ImgPart);
+
+      public override string ToString () => $"{RealPart} + i{ImgPart}";
    }
    #endregion
 }
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,8 +1,13 @@
+using Training;
+
 namespace Test;
 
 [TestClass]
 public class UnitTest1 {
    [TestMethod]
    public void TestMethod1 () {
+      ComplexNumber m1 = new (3, 4);
+      ComplexNumber m2 = new (1, 2);
+      Assert.AreEqual (5, m1.Norm);
    }
 }
