88a948ecdf071cf049b6eae453adaaa06ed37e51
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2024-01-24 12:58:05 +0530

    Incomplete code Test01

--- Program.cs
+++ Program.cs
@@ -3,16 +3,33 @@
 // Copyright (c) Metamation India.                                                
 // ---------------------------------------------------------------------------------------
 // Program.cs                                                                     
-// 
-// 
-// 
+// Program1
 // ---------------------------------------------------------------------------------------
 namespace Training {
    #region class Program --------------------------------------------------------------------------
    internal class Program {
       #region Method ------------------------------------------------
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+      static void Main () {
+         var file = File.ReadLines ("C:/etc/input.txt");
+         int r = 12, b = 14, g = 13;
+         List<int> red = new (), blue = new (), green = new ();
+         List<int> validGames = new ();
+         foreach (var line in file) {
+            var split1 = line.Split (':');
+            var tries = split1[1].Split (";");
+            foreach (var trial in tries) {
+               var colors = trial.Split (",");
+               foreach (var color in colors) {
+                  var colorParts = color.Trim ().Split (" ");
+                  if (colorParts[1] == "red") red.Add (int.Parse (colorParts[0]));
+                  if (colorParts[1] == "blue") blue.Add (int.Parse (colorParts[0]));
+                  if (colorParts[1] == "green") green.Add (int.Parse (colorParts[0]));
+               }
+            }
+            if (red.All (x => x <= r) && blue.All (x => x <= b) && green.All (x => x <= g))
+               validGames.Add (int.Parse (split1[0].Split (' ')[1]));
+         }
+         Console.WriteLine (validGames.Sum ());
       }
       #endregion
    }
