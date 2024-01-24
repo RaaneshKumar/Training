dc2345ff16afebfcddd2b67c262621c8c210353b
RaaneshKumar <raaneshkumar.k.r@metamation.com>
2024-01-24 12:59:42 +0530

    Removed Unnecessary files

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -7,7 +7,6 @@ public class UnitTest1 {
    [TestMethod]
    public void TestMethod1 () {
       ComplexNumber m1 = new (3, 4);
-      ComplexNumber m2 = new (1, 2);
       Assert.AreEqual (5, m1.Norm);
    }
 }
